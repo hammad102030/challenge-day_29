0a187972964613a49a06ecb1a62874557a7e0544
M.hammad abbasi <161203782+hammad102030@users.noreply.github.com>
2024-08-26 13:17:31 +0500

    This is my day 29 challenge.

---
+++ main.ts
@@ -0,0 +1,79 @@
+// Day 29
+
+// *Question 1: Calculate the Product of Non-Zero Digits*
+
+// Description:
+
+// Write a TypeScript function that takes a number as input and returns the product of all non-zero digits in the number.
+
+// Example:
+// typescript
+// Input: 4056
+// Output: 120  // 4 * 5 * 6 = 120
+
+// Input: 1002
+// Output: 2  // 1 * 2 = 2
+
+function non_Zero_Digits(num: number): number {
+
+    let count = 1;
+
+    while(num > 0) {
+        let digits = num % 10
+
+        if (digits !== 0) {
+            count *= digits
+        }
+        num = Math.floor(num / 10)
+    }
+    return count;
+}
+
+console.log(non_Zero_Digits(4056));// Output: 120  // 4 * 5 * 6 = 120
+console.log(non_Zero_Digits(1002));// Output: 2  // 1 * 2 = 2
+
+
+// Hint: Loop through each digit, skip zeros, and multiply the rest.
+
+// ---
+
+// *Question 2: Find the Difference Between the Largest and Smallest Digit*
+
+// Description:
+
+// Write a TypeScript function that takes a number as input and returns the difference between its largest and smallest digits.
+
+// Example:
+// typescript
+// Input: 7593
+// Output: 6  // 9 - 3 = 6
+
+// Input: 12345
+// Output: 4  // 5 - 1 = 4
+
+
+// Hint: Convert the number to a string or use a loop to identify the largest and smallest digits, then calculate their difference.
+
+function largest_And_Smallest(num1: number): number {
+
+    let largest_Digits = -Infinity;
+    let smallest_Digits = Infinity;
+
+    while (num1 > 0) {
+        let digits = num1 % 10
+
+        if(digits > largest_Digits) {
+           largest_Digits = digits
+        }
+
+        if(digits < smallest_Digits) {
+            smallest_Digits = digits
+        }
+
+        num1 = Math.floor(num1 / 10)
+    }
+    return largest_Digits - smallest_Digits
+};
+
+console.log(largest_And_Smallest(7593));// Output: 6  // 9 - 3 = 6
+console.log(largest_And_Smallest(12345));// Output: 4  // 5 - 1 = 4
